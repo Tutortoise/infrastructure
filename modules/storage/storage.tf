c59aaeeee406dbe73eae96ea6b32a4a02c10dc37
Faidil <faidilf@proton.me>
2024-11-26 17:04:11 +0800

    feat(storage): rename reference name to bucket and make bucket publicly accessible

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,5 +1,12 @@
-resource "google_storage_bucket" "gcs" {
+resource "google_storage_bucket" "bucket" {
   name          = "tutortoise-bucket"
   location      = var.location
   force_destroy = true
+}
+
+resource "google_storage_bucket_acl" "bucket_acl" {
+  bucket = google_storage_bucket.bucket.name
+  role_entity = [ 
+    "READER:allUsers",
+  ]
 }
