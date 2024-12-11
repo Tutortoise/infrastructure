03c3ac8dfa2e5de147a50b9d198a3ada2c873325
Faidil <faidilf@proton.me>
2024-12-11 20:15:14 +0800

    feat(storage): add storage bucket for storing model

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -4,8 +4,19 @@ resource "google_storage_bucket" "bucket" {
   force_destroy = false
 }
 
+resource "google_storage_bucket" "model" {
+  name          = "tutortoise-model"
+  location      = var.location
+  force_destroy = false
+}
+
 resource "google_storage_bucket_iam_member" "all_users" {
-  bucket = google_storage_bucket.bucket.name
+  for_each = toset([
+    google_storage_bucket.bucket.name,
+    google_storage_bucket.model.name
+  ])
+
+  bucket = each.value
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
